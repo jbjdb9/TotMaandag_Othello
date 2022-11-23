9356aabc6e94521380390f50b997b299d48a1ebc
jeroen <j.b.j.de.boer@st.hanze.nl>
2022-11-23 12:15:18 +0100

    remote & connect dingen gedaan

--- src/Connect.java
+++ src/Connect.java
@@ -1,50 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Connect {
-}
 
 
 
 
-    public static void handleNotInGame(String[] responseParts, String info) {
-        //SVR GAME MATCH {PLAYERTOMOVE: "<naam speler1>", GAMETYPE: "<speltype>", OPPONENT: "<naam tegenstander>"}
-        if (responseParts != null && responseParts[0].contains("MATCH") && info.contains("Othello")) {
-            //inGame = true;
-            // naam van ons team, moet gelijk zijn aan inlognaam
-            if (info.contains("itv2ag1")) {
-                //Model.player = 0;
-
-            }
-            else {
-                //Model.player = 1;
-
-            }
+    private static boolean login(PrintWriter out, BufferedReader in) {
+        out.println("login " + "itv2ag1"); // inlognaam moet geleik zijn met naam in info.contains bij if statement in handleNotInGame
+        String response;
+        try {
+            response = in.readLine();
+        } catch (IOException e) {
+            out.println("Login failed: no response");
+            return false;
         }
-    }
-
-    public static boolean handleInGame(String[] responseParts, String info, PrintWriter out) {
-        //SVR GAME YOURTURN {TURNMESSAGE: "<bericht voor deze beurt>"}
-        if (responseParts != null && responseParts[0].contains("YOURTURN")) {
-            //Model.player = 1;
-            //int move = AI.aiMove();
-            //out.println("move " + move);
-        }
-
-        //overbodige case, case eronder vangt deze al op (nog laten staan voor test)
-        //SVR GAME MOVE {PLAYER: "<speler>", DETAILS: "<reactie spel op zet>", MOVE: "<zet>"}
-        //else if (responseParts != null && responseParts[0].contains("MOVE") && info.contains("itv2ag1")) {
-        //Model.player = 0;
-        //}
-
-        else if (responseParts != null && responseParts[0].contains("MOVE") && !info.contains("itv2ag1")) {
-            //Model.player = 0;
-            //int zet = info.lastIndexOf("MOVE: " + 2);
-            //Model.updateBoard(Integer.parseInt(info.substring(zet)));
+        if (!response.equals("OK") && !response.equals("New Game Server [Version 1.0]")) {
+            out.println("Login failed: " + response);
+            return false;
+        } else {
+            out.println("Login successful");
         }
         return true;
-
     }
 }
-        //OK (jouw beurt voor een zet)
-
-        //SVR GAME <speler resultaat> {PLAYERONESCORE: "<score speler1>", PLAYERTWOSCORE: "<score speler2>", COMMENT: "<commentaar op resultaat>"}
--- src/Remote.java
+++ src/Remote.java
@@ -1,2 +1,46 @@
+import java.io.PrintWriter;
+
 public class Remote {
+
+
+
+
+    public static void handleNotInGame(String[] responseParts, String info) {
+        //SVR GAME MATCH {PLAYERTOMOVE: "<naam speler1>", GAMETYPE: "<speltype>", OPPONENT: "<naam tegenstander>"}
+        if (responseParts != null && responseParts[0].contains("MATCH") && info.contains("Othello")) {
+            //inGame = true; ---- inGame functie moet eerst gemaakt worden
+            // naam van ons team, moet gelijk zijn aan inlognaam
+            if (info.contains("itv2ag1")) {
+                //Model.player = 0; ---- oude lijn vanuit tic-tac-toe. zet wie aan de beurt is onze ai of tegenstander (human)
+
+            }
+            else {
+                //Model.player = 1; ---- zelfde als if statement
+
+            }
+        }
+    }
+
+    public static boolean handleInGame(String[] responseParts, String info, PrintWriter out) {
+        //SVR GAME YOURTURN {TURNMESSAGE: "<bericht voor deze beurt>"}
+        if (responseParts != null && responseParts[0].contains("YOURTURN")) {
+            //Model.player = 1;  ---- zelfde als voorgaande
+            //int move = AI.aiMove(); ---- roept ai aan om zet te doen, alleen aanroep functie verandert afhangend van naam
+            //out.println("move " + move); ---- stuur move door naar server, move naam hangt af van hoe we die in ai noemen
+        }
+
+        //overbodige case, case eronder vangt deze al op (nog laten staan voor test)
+        //SVR GAME MOVE {PLAYER: "<speler>", DETAILS: "<reactie spel op zet>", MOVE: "<zet>"}
+        //else if (responseParts != null && responseParts[0].contains("MOVE") && info.contains("itv2ag1")) {
+        //Model.player = 0;
+        //}
+
+        else if (responseParts != null && responseParts[0].contains("MOVE") && !info.contains("itv2ag1")) {
+            //Model.player = 0; ---- zelfde als voorgaande
+            int zet = info.lastIndexOf("MOVE: " + 2);
+            //Model.updateBoard(Integer.parseInt(info.substring(zet))); ---- zet de move van tegenstander in het board, hangt af van benaming rest code
+        }
+        return true;
+
+    }
 }
