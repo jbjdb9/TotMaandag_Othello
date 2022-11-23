abade519f91fe550d665ac257b8cdb493f425696
Matthias <matthiasnijman@hotmail.com>
2022-11-23 11:54:35 +0100

    Start for Play & refactor Local -> Game

--- src/Play.java
+++ src/Play.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class Play {
+    static Scanner input = new Scanner(System.in);
+    static boolean remote = false;
+    static boolean replay = false;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to Othello!");
+
+    }
+
+    public static void remote() {
+        System.out.println("Do you want to play local or remote? [local/remote]");
+        String result = input.nextLine();
+        switch (result.toLowerCase()){
+            case "remote" -> {
+                remote = true;
+            }
+            case "local" -> {
+                remote = false;
+            }
+            default -> {
+                System.out.println("Do you want to play local or remote? [local/remote]");
+                remote();
+            }
+        }
+    }
+
+    public static void replay() {
+        String result = input.nextLine();
+        switch (result.toUpperCase()){
+            case "Y" -> {
+                System.out.println("Play again!");
+                //Model.clearBoard();
+                //gameWin = false;
+            }
+            case "N" -> {
+                //playAgain = false;
+                System.out.println("Thank you for playing! See you next time!");
+            }
+            default -> {
+                System.out.println("Do you want to play again? [Y/N]");
+                replay();
+            }
+        }
     }
 }
