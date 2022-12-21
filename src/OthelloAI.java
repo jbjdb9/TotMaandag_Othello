371d44348fa9e3caf2adfd15797e4fe0a3d6e56f
StandeMan <s.freese@st.hanze.nl>
2022-12-21 20:35:26 +0100

    Eerste delen van de AI gemaakt.  - Stroking my dick :) I got lotion on my dick  - I'm horny as fuck  - En de punten toegevoegd per positie ;)

--- src/OthelloAI.java
+++ src/OthelloAI.java
@@ -28,8 +28,8 @@ public class OthelloAI {
         for (int row=0; row<8; row++){
             for (int col=0; col<8;col++){
                 if (OthelloReferee.validMove(new int[]{row, col})){
-                    moves[count][0] = col;
-                    moves[count][1] = row;
+                    moves[count][0] = col; //Y-0
+                    moves[count][1] = row; //X-1
                     count++;
                 }
             }
@@ -44,6 +44,80 @@ public class OthelloAI {
             }
         }
         return bestmove;
+    }
+
+    public static int[][] PositionScore(int[][] positions){
+        for(int pos=0; pos<positions.length-1;pos++){
+            //Hoeken
+            if (positions[pos][0] == 0 && positions[pos][1] == 0){ //A1
+                positions[pos][2] += 10;
+            }
+            if (positions[pos][0] == 7 && positions[pos][1] == 0){ //A8
+                positions[pos][2] += 10;
+            }
+            if (positions[pos][0] == 0 && positions[pos][1] == 7){ //H1
+                positions[pos][2] += 10;
+            }
+            if (positions[pos][0] == 7 && positions[pos][1] == 7){ //H8
+                positions[pos][2] += 10;
+            }
+///////////////////////////////////////////////////////////////////////////////////
+            // Stroken buitenkant boven horizontaal
+            for (int place=2;place<6;place++){ // C1 t/m F1
+                if (positions[pos][0] == 0 && positions[pos][1] == place){
+                    positions[pos][2] += + 3;
+                }
+            }
+            // Stroken buitenkant onder horizontaal
+            for (int place=2;place<6;place++){ // C8 t/m F8
+                if (positions[pos][0] == 7 && positions[pos][1] == place){
+                    positions[pos][2] += + 3;
+                }
+            }
+
+            // Stroken buitenkant links verticaal
+            for (int place=2;place<6;place++){ // A3 t/m A6
+                if (positions[pos][0] == place && positions[pos][1] == 0){
+                    positions[pos][2] += + 3;
+                }
+            }
+
+            // Stroken buitenkant rechts verticaal
+            for (int place=2;place<6;place++){ // H3 t/m H6
+                if (positions[pos][0] == place && positions[pos][1] == 7){
+                    positions[pos][2] += + 3;
+                }
+            }
+//////////////////////////////////////////////////////////////////////////////////////
+            // Stroken binnen aan de buitenkant boven
+            for (int place=2;place<6;place++){ // C2 t/m F2
+                if (positions[pos][0] == 1 && positions[pos][1] == place){
+                    positions[pos][2] -= + 3;
+                }
+            }
+
+            // Stroken binnen aan de onderkant boven
+            for (int place=2;place<6;place++){ // C7 t/m F7
+                if (positions[pos][0] == 6 && positions[pos][1] == place){
+                    positions[pos][2] -= + 3;
+                }
+            }
+
+            // Stroken binnen aan de zijkant links
+            for (int place=2;place<6;place++){ // B3 t/m B6
+                if (positions[pos][0] == place && positions[pos][1] == 1){
+                    positions[pos][2] -= + 3;
+                }
+            }
+
+            // Stroken binnen aan de zijkant rechts
+            for (int place=2;place<6;place++){ // G3 t/m G6
+                if (positions[pos][0] == place && positions[pos][1] == 6){
+                    positions[pos][2] -= + 3;
+                }
+            }
+        }
+        return positions;
     }
 }
 
